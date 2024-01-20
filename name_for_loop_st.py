27c1aa9d6562b023a427e95a8d6ddc54f2fe21f1
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-20 09:13:07 +0530

    Add files via upload

---
+++ name_for_loop_st.py
@@ -0,0 +1,40 @@
+# for loop for order vice for execute example
+str = "python"
+for i in str:
+    print(i)
+
+strs = "yogeesh"
+for i in strs:
+    print(i)
+
+string = "java"
+for i in string:
+     print(i)
+
+name = "html"
+for i in name:
+    print(i)
+
+names = "css"
+for i in names:
+    print(i)
+
+list = "java script"
+for i in list:
+    print(i)
+
+lists = "akshaya"
+for i in lists:
+    print(i)
+
+bracket = "arya"
+for i in bracket:
+    print(i)
+
+brackets = "devops"
+for i in brackets:
+    print(i)
+
+square = "engenearing"
+for i in square:
+    print(i)
